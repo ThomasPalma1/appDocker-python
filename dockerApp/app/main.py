4ac251ff64ce7c78d36e055f31d2900ba236758c
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-09-25 17:30:02 -0300

    creating a project: getting started
    - I am using a project that has already been developed as a basis for this;
    - the idea of this repository is to run the application using docker (upload the database and the backend).

---
+++ dockerApp/app/main.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://thomaspalma:qwerty-thomas@127.0.0.1/appDocker'
+db = SQLAlchemy(app)
+
+
+class Employee(db.Model):
+    __tablename__ = 'employee'
+    _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+
+db.create_all()
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        name = (request.form.get("name"))
+
+        if name:
+            f = Employee(name)
+            db.session.add(f)
+            db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/employees_list")
+def employees_list():
+    employees = Employee.query.all()
+
+    return render_template("list.html", employees=employees)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    employee = Employee.query.filter_by(_id=id).first()
+    db.session.delete(employee)
+    db.session.commit()
+
+    employees = Employee.query.all()
+
+    return redirect("/employees_list")
+    # return render_template("list.html", employees=employees)
+
+
+def redirect_user():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
